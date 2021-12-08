5df2b760696fdb77872ef0ccef5fcd5aa6a575ad
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-07 23:33:01 -0600

    add up and down migration functions

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments('cars_id') // is the one with `increments` automatically the primary key? or the first one with increments?
+    
+    table.text('vin', 17) // vin numbers are 17 characters
+      .notNullable().unique()
+    table.string('make', 255).notNullable()
+    table.string('model', 255).notNullable()
+    table.integer('mileage', 6).notNullable()
+    table.string('title', 255)
+    table.string('transmission', 255)
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
